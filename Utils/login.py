505bdc0d95b80cf464fecc91b6d59208715f42e3
Gabe Sucich <gabe.sucich@gmail.com>
2021-07-22 21:00:45 -0800

    Updating optionhistoricals with more error checking

--- Utils/login.py
+++ Utils/login.py
@@ -7,7 +7,7 @@ def login():
 	data_file = open(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + "/credentials.json", "r")
 	data = json.load(data_file)
 	data_file.close()
-	robin_stocks.login(data["username"], data["password"])
+	robin_stocks.login(data["username"], data["password"], expiresIn = 120000)
 	print("Logged In")
 
 login()
